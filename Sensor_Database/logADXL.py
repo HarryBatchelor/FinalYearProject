931ec05b0152a78e98c9e29694b30f891bb21aa2
Harry <batchelorh@cardiff.ac.uk>
2022-02-23 11:47:31 +0000

    Loggingg data

--- Sensor_Database/logADXL.py
+++ Sensor_Database/logADXL.py
@@ -2,7 +2,7 @@ import time
 import sqlite3
 import Adafruit_ADXL345
 
-dbname='sensorsData.db'
+dbname='sensorsdata.db'
 sampleFreq = 1 # time in seconds
 
 # get data from DHT sensor
@@ -20,16 +20,24 @@ def logData (x, y, z):
 	conn=sqlite3.connect(dbname)
 	curs=conn.cursor()
 	
-	curs.execute("INSERT INTO DHT_data values(datetime('now'), (?), (?), (?))", (x, y, z))
+	curs.execute("INSERT INTO ACC_data values(datetime('now'), (?), (?), (?))", (x, y, z))
 	conn.commit()
+	conn.close()
+
+def displayData():
+	conn=sqlite3.connect(dbname)
+	curs=conn.cursor()
+	print ("\nEntire database contents:\n")
+	for row in curs.execute("SELECT * FROM ACC_data"):
+		print (row)
 	conn.close()
 
 # main function
 def main():
-	while True:
-		x, y, z = getADXLdata()
-		logData (x, y, z)
-		time.sleep(sampleFreq)
+	for i in range (0,3):
+        getADXLdata()
+        time.sleep(sampleFreq)
+        displayData()
 
 # ------------ Execute program 
 main()
