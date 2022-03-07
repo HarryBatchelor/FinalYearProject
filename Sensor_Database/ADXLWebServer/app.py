93fd3686cb2cbfd1b7d3dac3d94f4376cabdb0ce
Harry <batchelorh@cardiff.ac.uk>
2022-03-07 11:44:55 +0000

    Needs html work I think

--- Sensor_Database/ADXLWebServer/app.py
+++ Sensor_Database/ADXLWebServer/app.py
@@ -5,6 +5,8 @@ import threading
 import os
 import sqlite3
 import Adafruit_ADXL345
+import pandas
+import matplotlib.pyplot as plt
 
 
 pi_camera = VideoCamera(flip = False) # flip pi camera if upside down
@@ -14,22 +16,6 @@ app = Flask(__name__)
 dbname='sensordata.db'
 sampleFreq = 1 #time in seconds
 
-
-#get data from sensor
-# def getADXLdata():
-# 	accel = Adafruit_ADXL345.ADXL345()
-
-# 	x, y, z = accel.read()
-# 	if x is not None and y is not None and z is not None:
-# 		return x, y, z
-
-# def logData(x, y, z):
-# 	conn=sqlite3.connect(dbname)
-# 	curs=conn.cursor()
-
-# 	curs.execute("INSERT INTO ACC_data values(datetime('now'), (?), (?), (?))", (x, y, z))
-# 	conn.commit()
-# 	conn.close()
 
 def main():
 	while True:
@@ -55,19 +41,27 @@ def getData():
 	return time, x, y, z, x2, y2, z2
 # main route 
 @app.route("/")
-def index():	
-	time, x, y, z, x2, y2, z2 = getData()
-	templateData = {
-		'time': time,
-		'x': x,
-		'y': y,
+def index():
+    time, x, y, z, x2, y2, z2 = getData()
+    templateData = {
+        'time': time,
+        'x': x,
+        'y': y,
         'z': z,
-		'x2': x2,
-		'y2': y2,
-        'z2': z2
-	}
-	return render_template('index.html', **templateData)
-
+        'x2': x2,
+        'y2': y2,
+        'z2': z2}
+    return render_template('index.html', **templateData)
+def PlotData():
+    sql = """SELECT timestamp, x, y, z from ACC_data ORDER BY timestamp DESC LIMIT 10"""
+    
+    data = pandas.read_sql(sql, conn)
+    plt.plot(data.timestamp, data.x, label = "X Coords")
+    plt.plot(data.timestamp, data.y, label = "Y Coords")
+    plt.plot(data.timestamp, data.z, label = "Z Coords")
+    plt.legend()
+    plt.title("Coords")
+    plt.show()
 
 @app.route('/video_feed')
 def video_feed():
