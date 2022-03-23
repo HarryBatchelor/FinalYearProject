b2252457135a5a43885422dbb478f82aa937e264
HarryBatchelor <hmbatchelor8@gmail.com>
2022-03-23 11:21:04 +0000

    CSS changges

--- Sensor_Database/SaveToCSV.py
+++ Sensor_Database/SaveToCSV.py
@@ -23,42 +23,22 @@ def PoleHit():
     curs.execute(Zstatment)
     output4 = curs.fetchall()
 
-    # X2statment = '''SELECT x2 FROM ACC_data ORDER BY timestamp DESC Limit 140 ;'''
-    # curs.execute(X2statment)
-    # output5 = curs.fetchall()
+    X2statment = '''SELECT x2 FROM ACC_data ORDER BY timestamp DESC Limit 140 ;'''
+    curs.execute(X2statment)
+    output5 = curs.fetchall()
 
-    # Y2statment = '''SELECT Y2 FROM ACC_data ORDER BY timestamp DESC Limit 140 ;'''
-    # curs.execute(Y2statment)
-    # output6 = curs.fetchall()
+    Y2statment = '''SELECT Y2 FROM ACC_data ORDER BY timestamp DESC Limit 140 ;'''
+    curs.execute(Y2statment)
+    output6 = curs.fetchall()
 
-    # Z2statment = '''SELECT Z FROM ACC_data ORDER BY timestamp DESC Limit 140 ;'''
-    # curs.execute(Z2statment)
-    # output7 = curs.fetchall()
+    Z2statment = '''SELECT Z FROM ACC_data ORDER BY timestamp DESC Limit 140 ;'''
+    curs.execute(Z2statment)
+    output7 = curs.fetchall()
 
     outputs = [output1, output2, output3, output4]
     
-    dictionary = {'TIME': output1, 'X': output2, 'Y': output3, 'Z': output4}
+    dictionary = {'TIME': output1, 'X': output2, 'Y': output3, 'Z': output4, 'X2': output5, 'Y2': output6, 'Z2': output7}
     dataframe = pd.DataFrame(dictionary)
     dataframe.to_csv('Hits.csv', index=False, mode='a')
-    # with open('Hits.csv', 'a', newline='') as csvfile:
-
-    #     fieldnames = ['TIME','X', 'Y', 'Z']
-
-    #     thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    #     thewriter.writeheader()
-
-        
-        
-
-        
-
-        # for time in output1:
-        #     thewriter.writerow({'TIME': time})
-        # for x in output2:
-        #     thewriter.writerow({'X': x})
-        # for y in output3:
-        #     thewriter.writerow({'Y': y})
-        # for z in output4:
-        #     thewriter.writerow({'Z': z})
 
 PoleHit()
