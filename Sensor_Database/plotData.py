783926cbdad5ebc75021d80802eec15b32b89e64
Harry Batchelor <hmbatchelor8@gmail.com>
2022-03-29 10:44:12 +0100

    data

--- Sensor_Database/plotData.py
+++ Sensor_Database/plotData.py
@@ -13,12 +13,12 @@ sql = """SELECT timestamp, X2, Y2, Z2, x, y ,z from ACC_data ORDER BY timestamp"
 data = pandas.read_sql(sql, conn)
 
 
-# plt.plot(data.timestamp, data.x, label = "X Coords")
-# plt.plot(data.timestamp, data.y, label = "Y Coords")
-# plt.plot(data.timestamp, data.z, label = "Z Coords")
-plt.plot(data.timestamp, data.X2, label = "X 2 Coords")
-plt.plot(data.timestamp, data.Y2, label = "Y 2 Coords")
-plt.plot(data.timestamp, data.Z2, label = "Z 2 Coords")
+plt.plot(data.timestamp, data.x, label = "X Coords")
+plt.plot(data.timestamp, data.y, label = "Y Coords")
+plt.plot(data.timestamp, data.z, label = "Z Coords")
+# plt.plot(data.timestamp, data.X2, label = "X 2 Coords")
+# plt.plot(data.timestamp, data.Y2, label = "Y 2 Coords")
+# plt.plot(data.timestamp, data.Z2, label = "Z 2 Coords")
 plt.legend()
 plt.title("Right Pole")
 plt.xlabel("Time")
