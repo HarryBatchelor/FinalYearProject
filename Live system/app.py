fc1d5f82b097f3926df92a06735393ef3c29c680
HarryBatchelor <hmbatchelor8@gmail.com>
2022-03-30 16:29:58 +0100

    live update

--- Live system/app.py
+++ Live system/app.py
@@ -1,43 +1,26 @@
-from socket import socket
-from flask import Flask, render_template, url_for, copy_current_request_context
-from flask_socketio import SocketIO, emit
+from crypt import methods
+from flask import Flask,render_template,url_for,request,redirect, make_response
+import random
+import json
+from time import time
 from random import random
-from time import sleep
-from threading import Thread, Event
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
 
-#turn the flask app into a socketio app
-socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
-
-thread = Thread()
-thread_stop_event = Event()
-
-def randomNumbergenerator():
-    print("making numbers")
-    while not thread_stop_event.is_Set():
-        number = round(random()*10, 3)
-        print(number)
-        socketio.emit('newnumber', {'number':number}, namespace='/test')
-        socketio.sleep(5)
-
-@app.route('/')
-def index():
+@app.route('/', methods=["GET", "POST"])
+def main():
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/test ')
-def test_connect():
-    global thread
-    print('Client connected')
-    
-    if not thread.isAlive():
-        thread = socketio.start_background_task(randomNumbergenerator)
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    Tempreature = random()*100
+    Humidity = random()*55
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
+    data = [time() *1000, Tempreature, Humidity]
 
-if __name__ == '___main__':
-    socketio.run(app)
+    response = make_response9json.dumps(data))
+    response.content_type = 'application/json'
+
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
