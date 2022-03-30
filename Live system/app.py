852322e6d2538d34e1ab0b1af693201c06d4ed38
HarryBatchelor <hmbatchelor8@gmail.com>
2022-03-30 16:54:58 +0100

    live test with adxl

--- Live system/app.py
+++ Live system/app.py
@@ -3,7 +3,11 @@ import random
 import json
 from time import time
 from random import random
+import Adafruit_ADXL345
+
 app = Flask(__name__)
+accel = Adafruit_ADXL345.ADXL345(address=0x53, busnum=1)
+# accel2 = Adafruit_ADXL345.ADXL345(address=0x53, busnum=4)
 
 @app.route('/', methods=["GET", "POST"])
 def main():
@@ -11,15 +15,16 @@ def main():
 
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    Tempreature = random()*100
-    Humidity = random()*55
+    while True:
+        x, y, z = accel.read()
+        # x2, y2, z2 = accel2.read()
+        data = [time()*1000, x, y, z]
+        
+        response = make_response(json.dumps(data))
 
-    data = [time() *1000, Tempreature, Humidity]
+        response.content_type = 'application/json'
 
-    response = make_response(json.dumps(data))
-    response.content_type = 'application/json'
-
-    return response
+        return response
 
 if __name__ == "__main__":
     app.run(host='192.168.0.150', port=5000, debug=True)
