c5c167f4eb12792792ceeb5d02b51e561ad6c065
Harry Batchelor <hmbatchelor8@gmail.com>
2022-04-09 16:15:16 +0100

    Sql needs more work

--- Sensor_Database/PlotFromHits.py
+++ Sensor_Database/PlotFromHits.py
@@ -1,23 +1,30 @@
+from enum import unique
 import sqlite3
-from turtle import left
 import matplotlib.pyplot as plt
 import pandas
 import csv
+from matplotlib.backends.backend_pdf import PdfPages
+import uuid
 dbname= 'sensorsdata.db'
 conn=sqlite3.connect(dbname)
 conn.row_factory = lambda curs, row: row[0]
 curs = conn.cursor()
-curs.execute("SELECT TimeOfHit.timestamp FROM ACC_data INNER JOIN TimeOfHit ON ACC_data.timestamp = TimeOfHit.timestamp WHERE pole = 'LEFT';")
+curs.execute("SELECT DISTINCT TimeOfHit.timestamp FROM ACC_data INNER JOIN TimeOfHit ON ACC_data.timestamp = TimeOfHit.timestamp WHERE pole = 'LEFT' ;")
 leftHits = curs.fetchall()
 # print(leftHits)
-
-for x in range(len(leftHits)):
-    conn=sqlite3.connect(dbname)
-    conn.row_factory = lambda cursor, row: row[0]
+uniquefilename = str(uuid.uuid4()) + '.pdf'
+for x in leftHits:
+    # conn.row_factory = lambda cursor, row: row[0]
     c = conn.cursor()
-    hits = c.execute('SELECT * from ACC_data WHERE timestamp != (?) LIMIT 10', (leftHits,))
+    hits = c.execute('SELECT x from ACC_data WHERE timestamp = (?) ORDER BY timestamp DESC LIMIT 10', (x,))
     hits = c.fetchall()
     print(hits)
+    # for times in leftHits:
+    #     fig = plt.figure()
+    #     plt.plot(hits) 
+    #     pdf = PdfPages(uniquefilename)
+    #     pdf.savefig(fig)
+    #     pdf.close()
     # curs.execute("""SELECT timestamp, x, y, z from ACC_data WHERE timestamp != "2022-03-29 18:17:47" AND x !="0" ORDER BY timestamp DESC LIMIT;""")
     # MoreLeftHits = curs.fetchall()
     # print(MoreLeftHits) 
@@ -26,17 +33,3 @@ for x in range(len(leftHits)):
     # curs.execute(sql,x)
     # MoreLeftHits = curs.fetchall()
     # print(MoreLeftHits)
-# data = pandas.read_sql(sql, conn)
-# print (data)
-# f = plt.figure()
-# plt.plot(data.timestamp, data.x, label = "X Coords")
-# plt.plot(data.timestamp, data.y, label = "Y Coords")
-# plt.plot(data.timestamp, data.z, label = "Z Coords")
-# plt.legend()
-# plt.title("Left Pole")
-# plt.xlabel("Time")
-# plt.ylabel("Accelration")
-# ax = plt.gca()
-# ax.axes.xasis.set_ticks([])
-# plt.show
-# f.savefig("foo.pdf")
