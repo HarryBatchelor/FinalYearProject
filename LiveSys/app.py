1f43882d6ab59d9bcf03b147a1f926d8609d47d9
HarryBatchelor <hmbatchelor8@gmail.com>
2022-04-14 15:36:14 +0100

    paiN

--- LiveSys/app.py
+++ LiveSys/app.py
@@ -11,11 +11,11 @@ from camera import VideoCamera
 app = Flask(__name__)
 accel = Adafruit_ADXL345.ADXL345(address=0x53, busnum=4)
 accel2 = Adafruit_ADXL345.ADXL345(address=0x53, busnum=4)
-# pi_camera = VideoCamera(flip=False)
+pi_camera = VideoCamera(flip=False)
 
 @app.route('/', methods=["GET", "POST"])
 def main():
-    return render_template('index3.html')
+    return render_template('index.html')
 
 @app.route('/data', methods=["GET", "POST"])
 def data():
@@ -29,20 +29,20 @@ def data():
         response.content_type = 'application/json'
 
         return response
-# @app.route('/camera')
-# def LiveStream():
-#     return render_template('LiveStream.html')
-# def gen(camera):
-#     #get camera frame
-#     while True:
-#         frame = camera.get_frame()
-#         yield(b'--frame\r\n'
-#                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/camera')
+def LiveStream():
+    return render_template('LiveStream.html')
+def gen(camera):
+    #get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen(pi_camera),
-#                         mimetype = 'multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                        mimetype = 'multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
