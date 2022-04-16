6f508588068a018ee25fd98c2f7153cab72e5c30
Harry Batchelor <hmbatchelor8@gmail.com>
2022-04-16 17:34:58 +0100

    I GET THE TIMESTAMP

--- Sensor_Database/PlotFromHits.py
+++ Sensor_Database/PlotFromHits.py
@@ -11,22 +11,23 @@ conn=sqlite3.connect(dbname)
 # Set output to be a list
 conn.row_factory = lambda curs, row: row[0]
 curs = conn.cursor()
-curs.execute("SELECT ACC_data.ROWID FROM TimeOfHit INNER JOIN ACC_data ON  TimeOfHit.ROWID = ACC_data.ROWID WHERE pole = 'LEFT' ;") #Execute SQL
+curs.execute("SELECT TimeOfHit.Row_ID FROM ACC_data INNER JOIN TimeOfHit ON ACC_data.ROWID = TimeOfHit.Row_ID WHERE pole = 'LEFT' ;") #Execute SQL
 leftHits = curs.fetchall()
 print (leftHits)
 # uniquefilename = str(uuid.uuid4()) + '.pdf'
 
 
-    
+    # 10:42:24
 
 #Collect the extended data for each timestamp above
-# for x in leftHits:
-#     c = conn.cursor()
-#     timestamp = c.execute('SELECT x from ACC_data WHERE ROWID BETWEEN (?) AND (?) ORDER BY timestamp DESC LIMIT 100 OFFSET (?)', (x-30, x + 30, x))
-    
-#     # xs = c.execute('SELECT x FROM ACC_data WHERE ')
-    # hits = c.fetchall()
-    # print(hits)
+for x in leftHits:
+    first = (x-30)
+    second = (x-300)
+    c = conn.cursor()
+    timestamp = c.execute('SELECT timestamp from ACC_data WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 100', (x, ))
+    x = c.execute('SELECT x from ACC_data WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 100', (x, ))
+    hits = c.fetchall()
+    print(hits)
     # for plots in hits:    
     #     fig = plt.figure()
     #     plt.plot(hits) 
--- Sensor_Database/SAVE_TimeOfHit.py
+++ Sensor_Database/SAVE_TimeOfHit.py
@@ -7,14 +7,14 @@ def PoleHitLEFT():
     conn=sqlite3.connect(dbname)
     curs = conn.cursor()
 
-    curs.execute("INSERT INTO TimeOfHit values (datetime('now'),('LEFT'))")
+    curs.execute("INSERT INTO TimeOfHit values ((SELECT timestamp FROM ACC_data ORDER BY timestamp DESC LIMIT 1), ('LEFT'), (SELECT ROWID FROM ACC_data ORDER BY timestamp DESC LIMIT 1))")
     conn.commit()
     conn.close()
 def PoleHitRIGHT():
     conn=sqlite3.connect(dbname)
     curs = conn.cursor()
 
-    curs.execute("INSERT INTO TimeOfHit values (datetime('now'), ('RIGHT'))")
+    curs.execute("INSERT INTO TimeOfHit values ((SELECT timestamp FROM ACC_data ORDER BY timestamp DESC LIMIT 1), ('RIGHT'), (SELECT ROWID FROM ACC_data ORDER BY timestamp DESC LIMIT 1))")
     conn.commit()
     conn.close()
 PoleHitRIGHT()
