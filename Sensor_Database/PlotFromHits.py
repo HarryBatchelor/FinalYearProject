2470e77023bfe314582d55f12b1fabd2db21a79a
HarryBatchelor <hmbatchelor8@gmail.com>
2022-04-24 18:54:53 +0100

    graphs done

--- Sensor_Database/PlotFromHits.py
+++ Sensor_Database/PlotFromHits.py
@@ -21,49 +21,44 @@ def PlotLeft():
         cX = conn.cursor()
         cY = conn.cursor()
         cZ = conn.cursor()
-        x = cX.execute('SELECT x from TEST WHERE ROWID > (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
-        y = cY.execute('SELECT y from TEST WHERE ROWID > (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
-        z = cZ.execute('SELECT z from TEST WHERE ROWID > (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
+        x = cX.execute('SELECT x from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
+        y = cY.execute('SELECT y from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
+        z = cZ.execute('SELECT z from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
         xs = cX.fetchall()
         ys = cY.fetchall()
         zs = cZ.fetchall()
     # Plot data
+        
         for i in leftHits:
+            
                 # plot 1
-            # plt.subplot(3,1,1)
-            # plt.plot(xs)
-            # plt.title("X Coords")
-            # plt.xticks([])
-            # plt.ylim([-500,500])
-            #     # plot 2
-            # plt.subplot(3,1,2)
-            # plt.plot(ys)
-            # plt.title("Y Coords")
-            # plt.xticks([])
-            # plt.ylim([-500,500])
-            #     # plot 3
-            # plt.subplot(3,1,3)
-            # plt.plot(zs)
-            # plt.title("Z Coords")
-            # plt.suptitle("Left Sensor")
-            # plt.xticks([])
-            # plt.ylim([-500,500])
-            plt.plot(xs, label = "X Coords")
-            plt.plot(ys, label = "Y Coords")
-            plt.plot(zs, label = "Z Coords")
-            # plt.legend()
-            plt.title("Right Pole")
-            plt.xlabel("Time")
-            plt.ylabel("Acceleration")
+            plt.subplot(3,1,1)
+            plt.plot(xs)
+            plt.title("X Coords")
+            plt.xticks([])
+            plt.ylim([-500,500])
+                # plot 2
+            plt.subplot(3,1,2)
+            plt.plot(ys)
+            plt.title("Y Coords")
+            plt.xticks([])
+            plt.ylim([-500,500])
+                # plot 3
+            plt.subplot(3,1,3)
+            plt.plot(zs)
+            plt.title("Z Coords")
+            plt.suptitle("Left Sensor")
+            plt.xticks([])
+            plt.ylim([-500,500])
             # give file unique name
             u = 1
             flname = "PDF/LEFT/" + str(u) + ".pdf"
             while path.exists(flname):
                 flname = "PDF/LEFT/" + str(u) + ".pdf"
                 u += 1
-        # plt.savefig(flname)
-    plt.show()
-    plt.close()
+        plt.savefig(flname)
+        # plt.show()
+        plt.close()
     
     print("Finished Left")
 def PlotRight():
@@ -84,9 +79,9 @@ def PlotRight():
         cX2 = conn.cursor()
         cY2 = conn.cursor()
         cZ2 = conn.cursor()
-        x2 = cX2.execute('SELECT x2 from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 100', (i, ))
-        y2 = cY2.execute('SELECT y2 from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 100', (i, ))
-        z2 = cZ2.execute('SELECT z2 from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 100', (i, ))
+        x2 = cX2.execute('SELECT x2 from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
+        y2 = cY2.execute('SELECT y2 from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
+        z2 = cZ2.execute('SELECT z2 from TEST WHERE ROWID < (?) ORDER BY timestamp DESC LIMIT 500', (i, ))
         x2s = cX2.fetchall()
         y2s = cY2.fetchall()
         z2s = cZ2.fetchall()
@@ -122,4 +117,4 @@ def PlotRight():
         plt.close()
     print("Finished Right")
 PlotLeft()
-# PlotRight()
+PlotRight()
